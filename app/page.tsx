4994f80292879e3762ff1d01993bc6c386cba288
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-05-30 12:30:20 +0200

    here home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import { Hero } from "@/components/heroes/Hero";
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <main>
+      <Hero
+        key={"home"}
+        title="Hwanhoon Kim"
+        subtitle="Frontend Developer | React, Next"
+        content="I'm a passionate frontend developer with a strong focus on creating
+        visually stunning and user-friendly web applications. Let's bring your
+        ideas to life!"
+        firstButton={{ title: "View Portfolio", href: "/#projects" }}
+        secondButton={{ title: "Contact Me", href: "/#contact" }}
+      />
+    </main>
+  );
 };
 
 export default HomePage;
