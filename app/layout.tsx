7023813113bdf4367184c4b6d366fa09672a526e
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-05-30 11:04:18 +0200

    Mantine post css setup ```zsh npm install --save-dev postcss postcss-preset-mantine postcss-simple-vars ```
    created `postcss.config.cjs` with the content of
    ```js
    module.exports = {
      plugins: {
        'postcss-preset-mantine': {},
        'postcss-simple-vars': {
          variables: {
            'mantine-breakpoint-xs': '36em',
            'mantine-breakpoint-sm': '48em',
            'mantine-breakpoint-md': '62em',
            'mantine-breakpoint-lg': '75em',
            'mantine-breakpoint-xl': '88em',
          },
        },
      },
    };
    ```
    
    added `ColorSchemeScript`, `MantineProvider` on `layour.tsx`(root)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { Arimo } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const arimo = Arimo({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-arimo",
+});
+import "@mantine/core/styles.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body className={arimo.className}>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
