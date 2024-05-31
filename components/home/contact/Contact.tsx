bac820d61ec29c6ab2b1eaf0cdb81c70521a1a4f
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-05-31 18:06:06 +0200

    contact me sending mail done with nodemailer

--- components/home/contact/Contact.tsx
+++ components/home/contact/Contact.tsx
@@ -1,9 +1,34 @@
+"use client";
+
 import { SectionType1 } from "@/components/sections/SectionType1";
 import { Button, Divider, TextInput, Textarea } from "@mantine/core";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export const Contact = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const sendMail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
+
+    const { error } = await fetch("/contact/send-mail", {
+      method: "post",
+      body: formData,
+    }).then((r) => r.json());
+
+    if (error) {
+      alert("error");
+      console.log(error);
+    } else {
+      alert("success");
+    }
+  };
   return (
     <SectionType1
       key={"contact"}
@@ -13,10 +38,19 @@ export const Contact = () => {
       below and I'll get back to you as soon as possible."
       twClassNameForSectionBackGround="contact-bg"
     >
-      <form className="mx-auto mt-8 max-w-md space-y-4">
+      <form
+        className="mx-auto mt-8 max-w-md space-y-4"
+        onSubmit={(e) => sendMail(e)}
+      >
         <div className="grid gap-4 sm:grid-cols-2">
           <div className="space-y-2">
-            <TextInput label="name" placeholder="John Doe" required />
+            <TextInput
+              label="name"
+              placeholder="John Doe"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="space-y-2">
             <TextInput
@@ -24,11 +58,19 @@ export const Contact = () => {
               placeholder="john@example.com"
               required
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
         </div>
         <div className="space-y-2">
-          <Textarea label="message" placeholder="Content" required />
+          <Textarea
+            label="message"
+            placeholder="Content"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
         <Button className="w-full" type="submit" style={{ width: "100%" }}>
           Send Message
@@ -40,7 +82,6 @@ export const Contact = () => {
           <h3 className="text-2xl font-semibold">For hire?</h3>
           <p className="">Download my CV now</p>
         </div>
-
         <Button
           variant="outline"
           component={Link}
---
+++ lib/mail/nodeMailerApi.ts
@@ -0,0 +1,15 @@
+import "server-only";
+
+const nodemailer = require("nodemailer");
+
+const nodeMailerApi = nodemailer.createTransport({
+  host: "mail.spacemail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_ADDRESS,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+export default nodeMailerApi;
