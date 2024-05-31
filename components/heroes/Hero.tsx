9fc5f956ece4705c0deff5c74b2d5ce303a1d3b2
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-05-31 18:36:02 +0200

    tailwind plugin added for text shadow

--- components/heroes/Hero.tsx
+++ components/heroes/Hero.tsx
@@ -21,14 +21,19 @@ export const Hero = ({
 }: HeroProps) => {
   return (
     <section
-      className={`flex items-center justify-center py-20 md:py-32 ${twClassNameForBackground}`}
+      className={`relative flex items-center justify-center py-20 md:py-32 ${twClassNameForBackground}`}
     >
-      <div className="container space-y-6 px-4 text-center md:px-6">
-        <h1 className="text-4xl font-bold tracking-tight decoration-wavy md:text-6xl">
+      <div className="absolute left-0 top-0 z-10 h-full w-full bg-[#33333388]"></div>
+      <div className="container z-20 space-y-6 px-4 text-center md:px-6">
+        <h1 className="text-shadow-outline text-4xl font-bold tracking-tight text-white decoration-wavy md:text-6xl ">
           {title}
         </h1>
-        <p className="text-xl text-gray-800 md:text-2xl ">{subtitle}</p>
-        <p className="mx-auto max-w-[600px] text-gray-800 ">{content}</p>
+        <p className="text-shadow-outline text-xl font-semibold text-gray-200 md:text-2xl ">
+          {subtitle}
+        </p>
+        <p className="text-shadow-outline mx-auto max-w-[600px] font-medium text-gray-200">
+          {content}
+        </p>
         <div className="flex justify-center gap-4">
           <Button
             className="hover-effect-mild"
