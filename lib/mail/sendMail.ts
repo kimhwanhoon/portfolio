096a59fe0a7a4031570c243a1ec7d5d8eaa6b6f3
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-06-02 00:12:37 +0200

    bug fixing

--- lib/mail/sendMail.ts
+++ lib/mail/sendMail.ts
@@ -17,23 +17,21 @@ const sendMail = async ({ email, name, message }: MailType) => {
 
   const emailHtml = render(ContactMeMail({ email, name, message }));
 
-  try {
-    const mailOption = {
-      from: `'김환훈⭐️' <${sender}>`,
-      to: recipient,
-      subject: title,
-      html: emailHtml,
-    };
+  const mailOption = {
+    from: `'김환훈⭐️' <${sender}>`,
+    to: recipient,
+    subject: title,
+    html: emailHtml,
+  };
 
-    const info = await nodeMailerApi.sendMail(mailOption);
+  const info = await nodeMailerApi.sendMail(mailOption);
 
-    if (info.accepted.length > 0) {
-      return { error: null, data: info };
-    } else {
-      return { error: true, data: info };
-    }
-  } catch (error) {
-    return { error: error, data: { sender, test: 1 } };
+  return { data: { info, sender, processEnv: process.env.MAIL_ADDRESS } };
+
+  if (info.accepted.length > 0) {
+    return { error: null, data: info };
+  } else {
+    return { error: true, data: info };
   }
 };
 
