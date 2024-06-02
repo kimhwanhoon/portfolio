3eba5aefa0e36cea3f69230b4420582859e37182
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-06-02 15:44:54 +0200

    imgs and svgs locally imported

--- components/home/skills/Skills.tsx
+++ components/home/skills/Skills.tsx
@@ -16,18 +16,18 @@ import {
 import { SkillsIcon } from "./SkillsIcon";
 
 const iconList = [
-  { Icon: IconHtml, name: "HTML 5" },
-  { Icon: IconBrandCss3, name: "CSS 3" },
-  { Icon: IconBrandJavascript, name: "JavaScript" },
-  { Icon: IconBrandTypescript, name: "TypeScript" },
-  { Icon: IconBrandReact, name: "React 18" },
-  { Icon: IconBrandNextjs, name: "Next App Router" },
-  { Icon: IconBrandGit, name: "Git" },
-  { Icon: IconBrandGithub, name: "GitHub" },
-  { Icon: IconTerminal2, name: "Zsh" },
-  { Icon: IconBrandTailwind, name: "Tailwind CSS" },
-  { Icon: IconBrandSupabase, name: "Supabase" },
-  { Icon: IconDatabase, name: "SQL" },
+  { src: "/skills/html.svg", name: "HTML 5" },
+  { src: "/skills/brand-css3.svg", name: "CSS 3" },
+  { src: "/skills/brand-javascript.svg", name: "JavaScript" },
+  { src: "/skills/brand-typescript.svg", name: "TypeScript" },
+  { src: "/skills/brand-react.svg", name: "React 18" },
+  { src: "/skills/brand-nextjs.svg", name: "Next App Router" },
+  { src: "/skills/brand-git.svg", name: "Git" },
+  { src: "/skills/brand-github.svg", name: "GitHub" },
+  { src: "/skills/terminal-2.svg", name: "Zsh" },
+  { src: "/skills/brand-tailwind.svg", name: "Tailwind CSS" },
+  { src: "/skills/brand-supabase.svg", name: "Supabase" },
+  { src: "/skills/database.svg", name: "SQL" },
 ];
 export const Skills = () => {
   return (
@@ -39,8 +39,14 @@ export const Skills = () => {
       twClassNameForSecondDiv="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-10 mt-8"
       twClassNameForSectionBackGround="bg-crosses"
     >
-      {iconList.map(({ Icon, name }, index) => (
-        <SkillsIcon key={`skills_icon_${index}`} Icon={Icon} name={name} />
+      {iconList.map(({ src, name }, index) => (
+        <SkillsIcon
+          key={`skills_icon_${index}`}
+          src={src}
+          name={name}
+          width={40}
+          height={40}
+        />
       ))}
     </SectionType1>
   );
