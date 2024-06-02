40f9037d2b76976445b5aeece446b0645bb492d1
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-06-02 16:00:19 +0200

    font imported from local not google server

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { ColorSchemeScript } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 
+import localFont from "@next/font/local";
+
 import { Arimo } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
@@ -14,11 +16,10 @@ import "@mantine/charts/styles.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const arimo = Arimo({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-arimo",
+const arimo = localFont({
+  src: "../public/fonts/Arimo-VariableFont_wght.ttf",
 });
+
 import { ThemeProvider } from "@/theme/ThemeProvider";
 import { Header } from "@/components/header/Header";
 
