c4bd85a40b1489b66ac8a0454c1058a73286e910
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-06-05 18:07:58 +0200

    logo logic changed due to usage of react scroll link

--- lib/Logo.tsx
+++ lib/Logo.tsx
@@ -1,25 +1,40 @@
 "use client";
 
 import { Button, ButtonProps } from "@mantine/core";
-import { Link } from "react-scroll";
+import NextLink from "next/link";
+import { Link as ReactScrollLink } from "react-scroll";
+import { usePathname } from "next/navigation";
 
 interface LogoProps {
   color: ButtonProps["color"];
 }
 
 export const Logo: React.FC<LogoProps> = ({ color }) => {
-  return (
-    <Button
-      style={{ fontSize: "1.25rem" }}
-      variant="transparent"
-      color={color}
-      component={Link}
-      to="home"
-      className="hover-effect text-2xl font-bold"
-      smooth
-      offset={-120}
-    >
-      KIMHWANHOON
-    </Button>
-  );
+  const pathname = usePathname();
+  const commonProps = {
+    style: { fontSize: "1.25rem" },
+    variant: "transparent",
+    color,
+    className: "hover-effect text-2xl font-bold",
+  };
+  if (pathname === "/") {
+    return (
+      <Button
+        component={ReactScrollLink}
+        to="home"
+        smooth
+        offset={-120}
+        {...commonProps}
+      >
+        KIMHWANHOON
+      </Button>
+    );
+  } else if (pathname === "/blog") {
+    return (
+      <Button component={NextLink} href={"/"} {...commonProps}>
+        KIMHWANHOON
+      </Button>
+    );
+  }
+  return null;
 };
