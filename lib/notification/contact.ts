38dab3a44fbb39161aedfbc18ad6a15399ac4e41
Hwanhoon Kim <kimhwanhoon@outlook.com>
2024-06-05 13:30:51 +0200

    contact me code cleaned

---
+++ lib/notification/contact.ts
@@ -0,0 +1,13 @@
+export const notificationContactError = {
+  title: "Error occurred",
+  message:
+    "Something went wrong! 🤥 Please try again or contact me directly at contact@hwanhoon.kim",
+  color: "red",
+  autoClose: 5000,
+};
+
+export const notificationContactSuccess = {
+  title: "Message sent!",
+  message: "Your message has been successfully sent to Hwanhoon Kim.",
+  autoClose: 5000,
+};
